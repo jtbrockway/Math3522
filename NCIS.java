199fe2986f9b923e65b661c9207cf7a778c328d3
Sierra Battan <sierra@battan.com>
2018-03-06 15:52:14 -0800

    inital commit

---
+++ NCIS.java
@@ -0,0 +1,26 @@
+
+public class NCIS {
+	
+	public static void main(String[] args) {
+		
+		// handle input parsing
+        
+        // while (!done)
+        
+            // if input == "done"
+        
+                // done with inputs
+        
+                // do stuff -- check if tridiagonal!
+        
+                    // check for wrong format
+        
+        // make sure that computation will work
+
+        // perform & print NCIS
+		
+        // close scanner
+		
+	}
+
+}
